7cc511a3297fbdc0408d7b490fd4545759bbd21b
chaithra47 <cbangera71@gmail.com>
2025-03-12 14:09:40 +0530

    Add files via upload

---
+++ touple.py
@@ -0,0 +1,10 @@
+# Creating a tuple
+my_tuple = (10, 20, 30, 40, 50)
+# Accessing elements
+print(my_tuple[0])  # Output: 10
+print(my_tuple[2])  # Output: 30
+# Slicing a tuple
+print(my_tuple[1:4])  # Output: (20, 30, 40)
+# Iterating through a tuple
+for item in my_tuple:
+    print(item)
